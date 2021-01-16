0b3ea0479418d13b1c12d39fd1c2591e3c295760
Jeremy Soller <jeremy@system76.com>
2021-01-15 20:49:36 -0700

    Build u-boot from source, WIP raspi3 target

--- aarch64.sh
+++ aarch64.sh
@@ -2,6 +2,62 @@
 
 set -ex
 
+MACHINE=virt
+
+U_BOOT="build/u-boot/${MACHINE}.bin"
+case "${MACHINE}" in
+	raspi3)
+		U_BOOT_CONFIG=rpi_3_defconfig
+		LOAD_ADDR=0x00000000
+		ENTRY_ADDR=0x00001000
+		IMAGE_ADDR=0x01000000
+		QEMU_ARGS=(
+			-M raspi3
+			-device "loader,file=build/kernel.uimage,addr=${IMAGE_ADDR},force-raw=on"
+			-kernel "${U_BOOT}"
+			-nographic
+			-serial null
+			-serial mon:stdio
+			-s
+		)
+		;;
+	virt)
+		U_BOOT_CONFIG=qemu_arm64_defconfig
+		LOAD_ADDR=0x40000000
+		ENTRY_ADDR=0x40001000
+		IMAGE_ADDR=0x41000000
+		QEMU_ARGS=(
+			-M virt
+			-cpu cortex-a57
+			-bios "${U_BOOT}"
+			-device "loader,file=build/kernel.uimage,addr=${IMAGE_ADDR},force-raw=on"
+			-nographic
+			-serial mon:stdio
+			-s
+		)
+		;;
+esac
+
+if [ ! -f "${U_BOOT}" ]
+then
+	make prefix
+
+	make -C u-boot distclean
+	make -C u-boot "${U_BOOT_CONFIG}"
+
+	sed -i \
+		's/^CONFIG_BOOTCOMMAND=.*$/CONFIG_BOOTCOMMAND="bootm '"${IMAGE_ADDR}"' - ${fdtcontroladdr}"/' \
+		u-boot/.config
+
+	TARGET=aarch64-unknown-redox
+	env CROSS_COMPILE="${TARGET}-" \
+		PATH="${PWD}/prefix/${TARGET}/relibc-install/bin/:${PATH}" \
+		make -C u-boot -j "$(nproc)"
+
+	mkdir -pv build/u-boot
+	cp -v u-boot/u-boot.bin "${U_BOOT}"
+fi
+
 mkdir -p build
 # rm -f build/libkernel.a build/kernel
 rm -f build/kernel
@@ -13,21 +69,14 @@ make build/initfs.tag
 
 mkimage \
 	-A arm64 \
-	-O "linux" \
+	-O linux \
 	-T kernel \
 	-C none \
-	-a 0x40000000 \
-	-e 0x40001000 \
-	-n 'Redox kernel (qemu AArch64 virt)' \
+	-a "${LOAD_ADDR}" \
+	-e "${ENTRY_ADDR}" \
+	-n "Redox kernel (qemu AArch64 ${MACHINE})" \
 	-d build/kernel \
    	build/kernel.uimage
 
-qemu-system-aarch64 \
-	-M virt \
-	-cpu cortex-a57 \
-	-bios u-boot.bin \
-	-device loader,file=build/kernel.uimage,addr=0x41000000,force-raw=on \
-	-serial mon:stdio \
-	-nographic \
-	-s \
-	"$@"
+qemu-system-aarch64 "${QEMU_ARGS[@]}" "$@"
+
